f8a724a5f01fc8be8da9a042c8367ef1597c2ddf
tmanthanwar <tmanthanwar@gmail.com>
2021-09-04 15:35:37 +0530

    example

---
+++ Number_nd_its_method.js
@@ -0,0 +1,14 @@
+var a =10.234567;
+
+console.log(a.toFixed(2));
+console.log(typeof a);
+console.log(a.toExponential(3));
+console.log(a.toPrecision(4));
+
+var b = 100;
+var c = new Number (100);
+ 
+console.log(typeof b);
+console.log(typeof c);
+console.log(b == c);
+console.log(b === c);
