1f2e0c10ee8bb24c05d998b0bf501ccd2f0cd3ec
tmanthanwar <tmanthanwar@gmail.com>
2022-01-13 13:05:05 +0530

    file added

---
+++ 123.js
@@ -0,0 +1,27 @@
+// remove duplicates
+
+// var array = ["ravi", "kiran","rajesh","kiran","rajesh"];
+// var newArray = [];
+
+// for(i=0; i<array.length; i++){
+//     if(newArray.includes(array[i])){
+//         continue;
+//     }
+//     newArray.push(array[i]);
+// }
+// console.log(newArray);
+// console.log(newArray.reverse());
+
+// // print string or other type of data
+
+// var array = [1,2,3,4,5,"ravi","kiran","swati",true,false];
+
+// function cleanArray(array, typeOfdata) {
+//     var newArray = array.filter(element);
+    // function (element) {
+//         return typeof element == typeOfdata;
+//     };
+//     console.log(newArray);
+// }
+// cleanArray(array,"number");
+
